673ba39e9b2d83a4fc39f4a91069836b17c7a98a
David Zhu <dzhu@brex.com>
2021-01-20 21:13:17 -0800

    improve formatting

--- FeedlyBackground/background.js
+++ FeedlyBackground/background.js
@@ -1,8 +1,8 @@
-(function() {
-    var receiveRequest = function(message, sender, response) {
-      chrome.tabs.create({url: message.url, active: false});
-    };
-    
-    // Listen for requests
-    chrome.runtime.onMessage.addListener(receiveRequest);
-  })();
+(function () {
+  var receiveRequest = function (message, sender, response) {
+    chrome.tabs.create({ url: message.url, active: false });
+  };
+
+  // Listen for requests
+  chrome.runtime.onMessage.addListener(receiveRequest);
+})();
--- FeedlyBackground/content.js
+++ FeedlyBackground/content.js
@@ -1,5 +1,5 @@
-(function() {
-    var keyListener = function(e) {
+(function () {
+    var keyListener = function (e) {
         if (!e.repeat && e.keyCode === 186) {
             // Find the selected frame's title link
             var targetURL = null
@@ -11,17 +11,17 @@
             } else if (openedArticle.length != 0) {
                 targetURL = openedArticle.attr('href')
             } else if (selectedArticle.length != 0) {
-                targetURL = selectedArticle.attr('href')  
+                targetURL = selectedArticle.attr('href')
             } else {
                 console.log("No target URLs found")
             }
 
-            if (targetURL != null){
-                chrome.runtime.sendMessage({url: targetURL});
+            if (targetURL != null) {
+                chrome.runtime.sendMessage({ url: targetURL });
             }
         }
     }
-  
+
     // Keydown only once
     window.addEventListener('keydown', keyListener, false);
-  })();
+})();
